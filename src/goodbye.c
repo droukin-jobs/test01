babf8f693fa3628451d19aeb8e44fb2ab6c556b6
D.Roukin <D.Roukin.Jobs@gmail.com>
2018-04-18 14:05:24 +1200

    added hello and goodbye files

---
+++ src/goodbye.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "goodbye.h"
+
+void goodbye(void){
+	printf("goodbye\n");
+}
--- src/main.c
+++ src/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include "hello.h"
+#include "goodbye.h"
+
 
 void main(void){
+	hello();
 	printf("main.c\n");
+	goodbye();
 }
